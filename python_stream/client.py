91184671fab38f9386aaa0ce491a8c679e312f9d
Christopher De Vries <devries@idolstarastronomer.com>
2020-07-10 08:59:46 -0400

    Add a streaming example in python

---
+++ python_stream/client.py
@@ -0,0 +1,33 @@
+import logging
+import argparse
+
+import grpc
+
+import primestream_pb2
+import primestream_pb2_grpc
+
+address = 'localhost:30301'
+
+def get_primes(stub, n):
+    request = primestream_pb2.PrimeCount(number=n)
+    for p in stub.GetPrimes(request):
+        logging.info(f"Received prime {p.count}: {p.value}")
+
+def main():
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+    parser = argparse.ArgumentParser(description="Get some primes")
+    parser.add_argument('-n', dest='number', default=5, type=int)
+    args = parser.parse_args()
+
+    with open('minica.pem', 'rb') as f:
+        creds = grpc.ssl_channel_credentials(f.read())
+
+    with grpc.secure_channel(address, creds) as channel:
+        try:
+            stub = primestream_pb2_grpc.PrimeStreamStub(channel)
+            r = get_primes(stub, args.number)
+        except grpc.RpcError as e:
+            logging.error(f"{e.code()}: {e.details()}")
+
+if __name__=='__main__':
+    main()
---
+++ python_stream/server.py
@@ -0,0 +1,73 @@
+from concurrent import futures
+import logging
+import math
+import itertools
+
+import grpc
+
+import primestream_pb2
+import primestream_pb2_grpc
+
+class PrimeStream(primestream_pb2_grpc.PrimeStreamServicer):
+    def GetPrimes(self, request, context):
+        p = context.peer()
+        logging.info(f"Received Request from {p}")
+        if request.number>100000:
+            logging.warning("Error: Asked for too many primes")
+            context.abort(grpc.StatusCode.INVALID_ARGUMENT, f"{request.number} is too many primes to return")
+        if request.number<0:
+            logging.warning("Error: Asked for negative amount")
+            context.abort(grpc.StatusCode.INVALID_ARGUMENT, "Requested number of primes must be positive")
+
+        for i, p in enumerate(primes(context, request.number)):
+            yield primestream_pb2.PrimeNumber(count=i, value=p)
+
+def serve():
+    with open('minica.pem', 'rb') as f:
+        root_cert = f.read()
+
+    with open('localhost/cert.pem', 'rb') as f:
+        cert = f.read()
+    
+    with open('localhost/key.pem', 'rb') as f:
+        private_key = f.read()
+
+    creds = grpc.ssl_server_credentials(((private_key, cert),),
+            root_certificates=root_cert)
+
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    primestream_pb2_grpc.add_PrimeStreamServicer_to_server(PrimeStream(), server)
+    server.add_secure_port('[::]:30301', creds)
+    server.start()
+    logging.info("Listening on port 30301")
+    server.wait_for_termination()
+
+def primes(context, nreq):
+    primes = []
+    yield 2
+
+    i = 3
+    ctr = 0
+    for i in itertools.count(3,2):
+        if not (context.is_active() and ctr<nreq):
+            break
+
+        isprime = True
+        iSqrt = int(math.floor(math.sqrt(i)))
+
+        for p in primes:
+            if i%p == 0:
+                isprime = False
+                break
+            if p > iSqrt:
+                break
+
+        if isprime:
+            primes.append(i)
+            ctr+=1
+            yield i
+
+if __name__=='__main__':
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+    serve()
+
