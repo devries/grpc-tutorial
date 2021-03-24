ca41bfbdcaa421b21ce1d5ef5066c31c84f192bf
Christopher De Vries <devries@idolstarastronomer.com>
2021-03-24 11:57:29 -0400

    Add arguments to query given addresses and ports to the python client

--- python_stream/client.py
+++ python_stream/client.py
@@ -6,7 +6,7 @@ import grpc
 import primestream_pb2
 import primestream_pb2_grpc
 
-address = 'localhost:30301'
+#address = 'localhost:30301'
 
 def get_primes(stub, n):
     request = primestream_pb2.PrimeCount(number=n)
@@ -17,7 +17,11 @@ def main():
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
     parser = argparse.ArgumentParser(description="Get some primes")
     parser.add_argument('-n', dest='number', default=5, type=int)
+    parser.add_argument('-a', dest='host', default='localhost')
+    parser.add_argument('-p', dest='port', default=30301)
     args = parser.parse_args()
+
+    address = f'{args.host}:{args.port}'
 
     with open('minica.pem', 'rb') as f:
         creds = grpc.ssl_channel_credentials(f.read())
